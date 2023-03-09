618b458dc8b8d640e6ca091dfc8fd65f8ff3da7b
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-09 09:13:46 +0100

    fetch api set up

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -3,9 +3,44 @@ import { useState, useEffect } from 'react';
 
 const Signup = ({ navigate }) => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const createUser = () => {
+    fetch("http://localhost:8080/users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: { email: email, password: password, username: username }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  }
+
   return (
     <div>
-    <h1>Welcome to the sign up page</h1>
+    <h1>Welcome to the sign up page.</h1>
+    <label>Email:</label>
+    <input type="text" onChange={handleEmail} />
+    <label>Password:</label>
+    <input type="password" onChange={handlePassword} />
+    <label>Username:</label>
+    <input type="text" onChange={handleUsername} />
+    <button>Sign up</button>
     </div>
   )
 }
