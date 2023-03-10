a5dc3bdf3da34b38a16af3207aa84c6c097b983d
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-10 13:12:07 +0100

    times all rendered as buttons

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
 const PostForm = () => {
@@ -7,6 +7,10 @@ const PostForm = () => {
   const [selectedCity, setSelectedCity] = useState();
   const [lat, setLat] = useState("");
   const [long, setLong] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  const [time, setTime] = useState(0);
+  const [showConditions, setShowConditions] = useState(true);
+  const isFirstRender = useRef(true);
 
   useEffect(() => {
     fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${selectedCity}`) 
@@ -19,13 +23,27 @@ const PostForm = () => {
   }, [selectedCity])
 
   useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,precipitation_probability,precipitation,rain,windspeed_10m&forecast_days=1`)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => setWeatherData(data.hourly))
   }, [lat, long])
 
   const handleSelectedCity = (event) => {
     setSelectedCity(event.target.value);
+  }
+
+  const getData = () => {
+    console.log(weatherData);
+    console.log(weatherData.temperature_2m[5]);
+    console.log(time);
+  }
+
+  const handleTime = (event) => {
+    setTime(event.target.value);
   }
 
   return (
@@ -39,7 +57,16 @@ const PostForm = () => {
       <option value="Manchester">Manchester</option>
       <option value="Rome">Rome</option>
     </select>
-    <button>get data</button>
+    <button onClick={getData}>get data</button>
+    {weatherData.time && weatherData.time.map((time, index) => 
+      <button key={index} value={index} onClick={handleTime}>{new Date(time).toLocaleTimeString()}</button>
+      )}
+    {showConditions &&
+      <div>
+      <div>{weatherData.rain && weatherData.rain[time]}</div>
+      <div>{weatherData.temperature_2m && weatherData.temperature_2m[time]}</div>
+      </div> 
+      }  
     </div>
 
   )
