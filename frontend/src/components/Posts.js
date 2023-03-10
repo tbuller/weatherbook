30e5b4855035fd46e707d77e770265f3e97e13c0
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-10 19:14:55 +0100

    Comments component created with post request

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import Comments from './Comments';
+import '../styling/Posts.scss';
 
 const Posts = () => {
 
@@ -13,7 +15,22 @@ const Posts = () => {
   return (
     <div>
     <div>
-    {posts.map(post => post.posterId)}    
+    {posts.map(post => 
+      <div key={post._id} className="post-container">
+      <div>{post.posterId}</div>
+      <div>{post.city}</div>
+      <div>{post.time}</div>
+      <div>{post.thoughts}</div>
+      {post.rain && <div>Rain: {post.rain}mm</div>}
+      {post.temperature && <div>Temperature: {post.temperature}°C</div>}
+      {post.precipitation && <div>Precipitation probability: {post.precipitation}%</div>}
+      {post.windspeed && <div>Wind speed: {post.windspeed} km/h</div>}
+      {post.radiation && <div>shortwave radiation: {post.radiation} W/m2</div>}
+      {post.cloudcover && <div>cloud cover: {post.cloudcover}%</div>}
+      <div>{post.createdAt}</div>
+      <Comments postId={post._id} />
+      </div>
+      )}    
     </div>
     <button onClick={showPosts}>show posts button</button>
     </div>
