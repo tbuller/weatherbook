1588455a8641238e73901ae93f85c14b56284436
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-13 11:40:16 +0100

    navbar styling and conditional navigation styling implemented

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styling/Navbar.scss';
 
@@ -6,12 +7,27 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
+  const [currentPage, setCurrentPage] = useState("");
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path === "/myhome") {
+      setCurrentPage("myhome");
+    } else if (path === "/profile") {
+      setCurrentPage("profile");
+    } else if (path === "/notifications") {
+      setCurrentPage("notifications");
+    } else if (path === "/messages") {
+      setCurrentPage("messages");
+    }
+  })
+
   return (
   <span className="navbar-container">
-  <button className="nav-home" onClick={() => navigate("/myhome")}>Home</button>
-  <button className="nav-profile" onClick={() => navigate("/profile")}>Profile</button>
-  <button className="nav-notifications" onClick={() => navigate("/notifications")}>Notifications</button>
-  <button className="nav-messages" onClick={() => navigate("/messages")}>Messages</button>
+  <button className={currentPage === "myhome" ? "nav-selected" : "nav-myhome"} onClick={() => navigate("/myhome")}>Home</button>
+  <button className={currentPage === "profile" ? "nav-selected" : "nav-profile"} onClick={() => navigate("/profile")}>Profile</button>
+  <button className={currentPage === "notifications" ? "nav-selected" : "nav-notifications"} onClick={() => navigate("/notifications")}>Notifications</button>
+  <button className={currentPage === "messages" ? "nav-selected" : "nav-messages"} onClick={() => navigate("/messages")}>Messages</button>
   </span>
   )
   
