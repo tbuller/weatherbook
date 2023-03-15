d00ab2d178209346b2b1ab2ce3aa23b9ba53343b
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-15 12:00:41 +0100

    date formatting

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -26,7 +26,7 @@ const Posts = () => {
       <div className="profile-picture"><CgProfile /></div>  
       <div className="username">{users.map(u => u._id === post.posterId && <p key={u._id}>{u.username}</p>)}</div>
       <div className="city">{post.city}</div>
-      <div className="time">{post.time}</div>
+      <div className="time">{(post.time).toString().slice(11, 16)}, {new Date(post.time).toDateString()}</div>
       </div>
       <div className="thoughts">{post.thoughts}</div>
       {post.rain && <div className="rain">Rain: {post.rain}mm</div>}
@@ -35,7 +35,7 @@ const Posts = () => {
       {post.windspeed && <div className="windspeed">Wind speed: {post.windspeed} km/h</div>}
       {post.radiation && <div className="radiation">shortwave radiation: {post.radiation} W/m2</div>}
       {post.cloudcover && <div className="cloudcover">cloud cover: {post.cloudcover}%</div>}
-      <div className="posted-at">{post.createdAt}</div>
+      <div className="posted-at">Posted at: {post.postedAt}</div>
       <Comments postId={post._id} />
       <CommentsForm postId={post._id} />
       </div>
