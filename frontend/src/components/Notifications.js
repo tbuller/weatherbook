45c6887a916a560b0b9a95b8cc3917f5b87e85dd
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-24 16:44:05 +0100

    requests rendered for each user that has sent the logged in user one

--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import RequestNotification from './RequestNotification';
 import PokeNotification from './PokeNotification';
 
 const Notifications = () => {
 
+  const [users, setUsers] = useState([]);
+  const [loggedInUser, setLoggedInUser] = useState({});
 
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data.users);
+        data.users.map(u => u._id === localStorage.getItem("userId") ? setLoggedInUser(u) : null);
+      })
+  }, [])
+
+  const showLoggedIn = () => {
+    console.log(loggedInUser);
+  }
 
   return (
     <div>
     <Navbar />
-    <h1>Notifications page</h1>
+    <h1>Your Notifications</h1>
+    <div>
+    {loggedInUser && loggedInUser?.requests?.length > 0 && <RequestNotification requests={loggedInUser.requests} users={users} />}  
+    <button onClick={showLoggedIn}>show logged in</button>
+    </div>
     </div>
   )
 }
--- frontend/src/components/RequestNotification.js
+++ frontend/src/components/RequestNotification.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const RequestNotification = () => {
+const RequestNotification = ({ requests, users }) => {
+
+  // const showRequests = () => {
+  //   console.log(requests);
+  // }
 
   return (
-    <div>Friend notification</div>
+    <div>
+    {requests.map(r => {
+      const requester = users.find(u => u._id === r)
+      return (
+      <div className="request-container" key={requester._id}>
+      <div>{requester.username}</div>
+      <div>{requester.email}</div>  
+      </div>
+      )
+      })}    
+    </div>
   )
 }
 
