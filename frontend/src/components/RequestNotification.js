72dc3cdaa1fe88fea9b71793a5c403358b115724
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-25 18:11:37 +0100

    added ability to view user's profile and then go back to notifications

--- frontend/src/components/RequestNotification.js
+++ frontend/src/components/RequestNotification.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import UserInfo from './UserInfo';
 import '../styling/RequestNotification.scss';
 
 const RequestNotification = ({ requests, users, loggedInUser }) => {
 
-  // const showRequests = () => {
-  //   console.log(requests);
-  // }
+  const [showRequester, setShowRequester] = useState(false);
+  const [viewedRequester, setViewedRequester] = useState({});
 
   const acceptRequest = (requesterId) => {
     fetch("http://localhost:8080/users/acceptrequest", {
@@ -20,18 +20,35 @@ const RequestNotification = ({ requests, users, loggedInUser }) => {
       .then(data => console.log(data))
   }
 
+  const viewUser = (user) => {
+    setShowRequester(!showRequester);
+    setViewedRequester(user);
+  }
+
+  const goBack = () => {
+    setShowRequester(!showRequester);
+    setViewedRequester({});
+  }
+
   return (
-    <div>
+    <div className="request-notification-page-container">
+    {!showRequester ? <div>
     {requests.map(r => {
       const requester = users.find(u => u._id === r)
       return (
       <div className="request-container" key={requester._id}>
       <img src={requester.photo || "/blank-photo.webp"} alt="profile-photo" className="profile-photo" />  
-      <div className="requester-username">{requester.username}</div>
+      <div className="requester-username" onClick={() => viewUser(requester)}>{requester.username}</div>
       <button className="accept-request-button" onClick={() => acceptRequest(requester._id)}>{loggedInUser.friends.includes(requester._id) ? "Request accepted" : "Accept Request"}</button>  
       </div>
       )
       })}    
+    </div> : 
+    <div>
+    <button onClick={goBack}>Go back to notifications</button>
+    <UserInfo user={viewedRequester} />  
+    </div>
+    }
     </div>
   )
 }
