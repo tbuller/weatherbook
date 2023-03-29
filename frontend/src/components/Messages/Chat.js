bc6b9998cc3b0258ccc2c641e0e857b4f0dd34f1
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-29 17:13:51 +0200

    chat styling and added input

--- frontend/src/components/Messages/Chat.js
+++ frontend/src/components/Messages/Chat.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import '../../styling/Chat.scss';
 
 const Chat = () => {
 
   const chats = useSelector(state => state.chats.chats);
+  const selectedChat = useSelector(state => state.chats.selectedChat);
 
   return (
     <div className="chats-container">
-
+    <input className="chat-input-field" type="text" />
     </div>
   )
 }
