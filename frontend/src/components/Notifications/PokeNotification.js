5debebfc8a784d6b32ac77124a6c562fd2bbedf0
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-04-09 11:36:31 +0100

    poke component initial fetch set up

--- frontend/src/components/Notifications/PokeNotification.js
+++ frontend/src/components/Notifications/PokeNotification.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const PokeNotification = () => {
 
+  const [poked, setPoked] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/pokes", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ pokerId: loggedInUser._id, pokeeId: pokeeId })
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [poked])
+
+  const handlePoke = () => {
+    setPoked(true);
+  }
+
   return (
-    <div>Poke</div>
+    <div>
+    <button>Poke</button>  
+    </div>
   )
 }
 
