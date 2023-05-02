71dd32cdedf7db43d7bb64864b1cd6e273c1b100
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-02 10:08:36 +0200

    landing page styled and given funtionality

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import '../styling/Landing.scss';
 
-const Landing = (navigate) => {
+const Landing = ({ navigate }) => {
+
+  const handleNavigateLogin = () => {
+    navigate("/login");
+  }
+
+  const handleNavigateSignup = () => {
+    navigate("/signup");
+  }
 
   return (
-    <div>Hello world</div>
+    <div className="landing-page-container">
+      <h1 className="landing-welcome">Welcome to Weatherbook</h1>
+      <h3 className="landing-prompt">Log in or sign up to continue</h3>
+      <button className="landing-page-button" onClick={handleNavigateLogin}>Log in</button>
+      <button className="landing-page-button" onClick={handleNavigateSignup}> Sign up</button>
+    </div>
   )
 } 
 
